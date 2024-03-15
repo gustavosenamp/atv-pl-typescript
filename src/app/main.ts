56f92b768f4da6d68f4432206698dfc19f4b4110
Gustavo Sena <123789443+gustavosenamp@users.noreply.github.com>
2024-03-15 10:42:19 -0300

    a

---
+++ src/app/main.ts
@@ -0,0 +1,47 @@
+import Entrada from "../io/entrada";
+import Empresa from "../modelo/empresa";
+import CadastroCliente from "../negocio/cadastroCliente";
+import CadastroProduto from "../negocio/cadastroProduto";
+import ListagemClientes from "../negocio/listagemClientes";
+import ListagemProdutos from "../negocio/listagemProdutos";
+
+console.log(`Bem-vindo ao melhor sistema de gerenciamento de pet shops e clínicas veterinarias`)
+let empresa = new Empresa()
+let execucao = true
+
+while (execucao) {
+    console.log(`Opções:`);
+    console.log(`1 - Cadastrar cliente`);
+    console.log(`2 - Listar todos os clientes`);
+    console.log(`3 - Cadastrar produto`);
+    console.log(`4 - Listar todos os Produtos`)
+    console.log(`0 - Sair`);
+
+    let entrada = new Entrada()
+    let opcao = entrada.receberNumero(`Por favor, escolha uma opção: `)
+
+    switch (opcao) {
+        case 1:
+            let cadastro = new CadastroCliente(empresa.getClientes)
+            cadastro.cadastrar()
+            break;
+        case 2:
+            let listagem = new ListagemClientes(empresa.getClientes)
+            listagem.listar()
+            break;
+        case 3:
+            let cadastroProduto = new CadastroProduto(empresa.getProdutos)
+            cadastroProduto.cadastrar()
+            break;
+        case 4:
+            let listagemProdutos = new ListagemProdutos(empresa.getProdutos)
+            listagemProdutos.listar()
+            break;
+        case 0:
+            execucao = false
+            console.log(`Até mais`)
+            break;
+        default:
+            console.log(`Operação não entendida :(`)
+    }
+}
