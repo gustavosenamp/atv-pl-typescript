8f845411edeebfd5f2b392e9bf090a5bcd511a59
Gustavo Sena <123789443+gustavosenamp@users.noreply.github.com>
2024-05-30 16:28:36 -0300

    conectando as listas e os formulários

--- atvii/src/componentes/roteador.tsx
+++ atvii/src/componentes/roteador.tsx
@@ -2,6 +2,12 @@ import { Component } from "react";
 import BarraNavegacao from "./barraNavegacao";
 import ListaCliente from "./listaClientes";
 import FormularioCadastroCliente from "./formularioCadastroCliente";
+import FormularioCadastroPet from "./formularioCadastroPet";
+import ListaPet from "./listaPets";
+import FormularioCadastroProduto from "./formularioCadastroProduto";
+import ListaProduto from "./listaProduto";
+import FormularioCadastroServico from "./formularioCadastroServico";
+import ListaServico from "./listaServico";
 
 type state = {
     tela: string
@@ -25,21 +31,71 @@ export default class Roteador extends Component<{}, state>{
     }
 
     render() {
-        let barraNavegacao = <BarraNavegacao seletorView={this.selecionarView} tema="#e3f2fd" botoes={['Clientes', 'Cadastros']} />
-        if (this.state.tela === 'Clientes') {
-            return (
-                <>
-                    {barraNavegacao}
-                    <ListaCliente tema="#e3f2fd" />
-                </>
-            )
-        } else {
-            return (
-                <>
-                    {barraNavegacao}
-                    <FormularioCadastroCliente tema="#e3f2fd" />
-                </>
-            )
+        let barraNavegacao = <BarraNavegacao seletorView={this.selecionarView} tema="#e3f2fd" botoes={['Clientes', 'Cadastros Clientes', 'Pets', 'Cadastros Pets', 'Produtos', 'Cadastros Produtos', 'Serviços', 'Cadastros Serviços']} />
+        switch (this.state.tela) {
+            case 'Clientes':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <ListaCliente tema="#e3f2fd" />
+                    </>
+                )
+            case 'Cadastros Clientes':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <FormularioCadastroCliente tema="#e3f2fd" />
+                    </>
+                )
+            case 'Pets':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <ListaPet tema="#e3f2fd" />
+                    </>
+                )
+            case 'Cadastros Pets':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <FormularioCadastroPet tema="#e3f2fd" />
+                    </>
+                )
+            case 'Produtos':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <ListaProduto tema="#e3f2fd" />
+                    </>
+                )
+            case 'Cadastros Produtos':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <FormularioCadastroProduto tema="#e3f2fd" />
+                    </>
+                )
+            case 'Serviços':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <ListaServico tema="#e3f2fd" />
+                    </>
+                )
+            case 'Cadastros Serviços':
+                return (
+                    <>
+                        {barraNavegacao}
+                        <FormularioCadastroServico tema="#e3f2fd" />
+                    </>
+                )
+            default:
+                return (
+                    <>
+                        {barraNavegacao}
+                        <ListaCliente tema="#e3f2fd" />
+                    </>
+                )
         }
     }
 }
